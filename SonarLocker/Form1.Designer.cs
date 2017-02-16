5df6a0448f4ec22efafa3ab404e5fe94d677d208
rpoore <rpoore@DESKTOP-RQINMGE>
2017-02-15 19:50:17 -0500

    Add project files.

---
+++ SonarLocker/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace SonarLocker
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTriggerDistance = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.cmboPort = new System.Windows.Forms.ComboBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "COM Port";
+            // 
+            // txtTriggerDistance
+            // 
+            this.txtTriggerDistance.Location = new System.Drawing.Point(16, 104);
+            this.txtTriggerDistance.Name = "txtTriggerDistance";
+            this.txtTriggerDistance.Size = new System.Drawing.Size(50, 20);
+            this.txtTriggerDistance.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Max Distance";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 168);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(286, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(180, 28);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(76, 47);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // cmboPort
+            // 
+            this.cmboPort.FormattingEnabled = true;
+            this.cmboPort.Location = new System.Drawing.Point(18, 44);
+            this.cmboPort.Name = "cmboPort";
+            this.cmboPort.Size = new System.Drawing.Size(121, 21);
+            this.cmboPort.TabIndex = 7;
+            this.cmboPort.SelectedValueChanged += new System.EventHandler(this.cmboPort_SelectedValueChanged);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(180, 27);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(76, 47);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 190);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.cmboPort);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTriggerDistance);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sonar Locker";
+            this.Shown += new System.EventHandler(this.frmMain_Shown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTriggerDistance;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ComboBox cmboPort;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ SonarLocker/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Diagnostics;
+
+namespace SonarLocker
+{
+    public partial class frmMain : Form
+    {
+        private static SerialPort IO;
+        static bool _continue = true;
+        static int MAXDIST = 30;
+        Thread readThread = null;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            btnStop.Visible = false;
+            btnStart.Visible = false;
+        }
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+            // get available serial ports
+            if (!ListSerPorts())
+            {
+                MessageBox.Show("No devices found.", "Oh Monkey Pickels!!");
+                Application.Exit();
+            }
+            else
+            {
+                txtTriggerDistance.Text = MAXDIST.ToString();
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            initSerialPort(cmboPort.GetItemText(cmboPort.SelectedItem));
+
+            toolStripStatusLabel1.Text = "Now sonaring through " + cmboPort.GetItemText(cmboPort.SelectedItem);
+
+            btnStart.Visible = false;
+            btnStop.Visible = true;
+
+            MAXDIST = int.Parse(txtTriggerDistance.Text);
+            txtTriggerDistance.ReadOnly = true;
+
+            // set the max distance for the device to MAXDIST
+            IO.WriteLine(MAXDIST.ToString());
+
+            readThread = new Thread(ReadPort);
+            readThread.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _continue = false;
+
+            Application.Exit();
+        }
+
+        private void cmboPort_SelectedValueChanged(object sender, EventArgs e)
+        {
+            btnStart.Visible = true;
+            toolStripStatusLabel1.Text = "Ready to sonar....";
+        }
+
+        public bool ListSerPorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            if (ports.Length > 0)
+            {
+                foreach (string port in ports)
+                {
+                    cmboPort.Items.Add(port);
+                }
+
+                return true;
+            }
+            else
+            {               
+                return false;
+            }
+        }
+
+        private static bool initSerialPort(string serialPortNum)
+        {
+            try
+            {
+                if (IO != null)
+                {
+                    IO.Close();  //Just in case port is already taken
+                }
+
+                IO = new SerialPort(serialPortNum, 9600, Parity.None, 8, StopBits.One);
+                IO.DtrEnable = false;
+                IO.Handshake = Handshake.None;
+                IO.RtsEnable = false;
+
+                IO.Open();
+
+                return true;
+            }
+            catch
+            {
+                serialPortNum = String.Empty;
+                IO.Close();
+
+                return false;
+            }
+        }
+
+        public static void ReadPort()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    // see if data has been sent
+                    if (IO.BytesToRead == 0)
+                    {
+                        string message = IO.ReadLine();
+                        
+                        // start screen saver if TRIG is received
+                        if (message.Contains("TRIG"))
+                        {
+                            ProcessStartInfo startInfo = new ProcessStartInfo("rundll32.exe");
+                            startInfo.Arguments = "user32.dll, LockWorkStation";
+                            Process.Start(startInfo);
+                        }
+                    }
+                }
+                catch (TimeoutException)
+                { }
+            }
+        }
+
+    }
+}
